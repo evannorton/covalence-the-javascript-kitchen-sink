28d492b32032b347149fcf603debdb21d5181eeb
Evan Norton <evanan39@yahoo.com>
2018-01-10 13:40:32 -0600

    step 6

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-//step 2
+//step 2: name variable
 let name = "Evan";
-//step 3
+//step 3: states constant
 const states = 50;
-//step 4
+//step 4: sum 5 and 4
 let sum = 5 + 4;
-//step 5
+//step 5: check if name starts with L, respond based on result
 if (name.indexOf("L") === 0) {
     alert("Back of the line!");
 } else {
